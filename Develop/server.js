c4cfca1104864c4d8095dd766ccc61bace162ca7
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-01-22 23:20:45 -0500

    created server.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,9 @@ const express = require('express');
 
 const PORT = 3001;
 const app = express();
+
+// GET request
+app.get('/notes', (req, res) => res.send('db/views/notes.html'))
+
+// POST request
+// Bonus: DELETE request
