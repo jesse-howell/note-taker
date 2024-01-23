90191088111870a447d96f8374bdd1506fdefa65
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-01-23 00:23:07 -0500

    installed uuid, added path module

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
 
 const PORT = 3001;
 const app = express();
 
+app.use(db.json());
 // GET HTML request routes
-app.get('/notes', (req, res) => res.send('/db/views/notes.html'));
-app.get('*', (req,res) => res.send('/db/views/index.html'));
-// POST request
+app.get('/notes', (req, res) => res.send('/views/notes.html'));
+app.get('*', (req,res) => res.send('/views/index.html'));
+
+// GET API request route
+// app.get('/api/notes', (req, res) => res.json('/db/db.json'))
+// POST API request route
+// app.post('/api/notes')
 // Bonus: DELETE request
 
 app.listen(PORT, () =>
