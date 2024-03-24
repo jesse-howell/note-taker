1ca5dc4e2a24fc35f290a825dfcec51d49f0f908
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-23 23:32:59 -0400

    added routes to server.js

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -2,4 +2,8 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
-// GET
+// GET route to return index.html
+// GET route to return notes.html
+// GET route to return all saved notes
+// POST route to receive new note to add to the req.body and serve to the client
+// DELETE route to delete a note by id
