5767a40d5bc10a4c2d861654519f07ca3ebab3d6
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-23 23:34:10 -0400

    deleted server-static.js

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const notes = require('express');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 // GET route to return index.html
+
 // GET route to return notes.html
 // GET route to return all saved notes
 // POST route to receive new note to add to the req.body and serve to the client
 // DELETE route to delete a note by id
+
+const uuid = () => {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
