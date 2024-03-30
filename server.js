663cd7fd3e97691ccfa9298daf9cdbf7bdb59303
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-30 00:13:34 -0400

    added urlencoded to server

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const PORT = 3001;
+const PORT = process.env.PORT ?? 3001;
 const path = require('path');
 const api = require('./routes/api/index.js');
 const app = express();
 
-app.use('api', api);
+app.use(express.json());
+app.use('./routes/api/index.js', api);
 // GET route to return index.html
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/views/index.html'))
+  res.sendFile(path.join(__dirname, 'views/index.html'))
 );
 
 // GET route to return notes.html
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/views/notes.html'))
+  res.sendFile(path.join(__dirname, 'views/notes.html'))
 );
 
 // DELETE route to delete a note by id
